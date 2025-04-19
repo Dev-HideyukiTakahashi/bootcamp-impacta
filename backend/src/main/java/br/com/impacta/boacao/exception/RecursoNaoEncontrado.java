6b9f1f994c43ef8f58ea96024786f6b2d36219c0
Hideyuki <dev.hideyukitakahashi@gmail.com>
2025-04-19 05:35:17 -0300

    recurso nao encontrado exception custom

---
+++ backend/src/main/java/br/com/impacta/boacao/exception/RecursoNaoEncontrado.java
@@ -0,0 +1,8 @@
+package br.com.impacta.boacao.exception;
+
+public class RecursoNaoEncontrado extends RuntimeException {
+
+    public RecursoNaoEncontrado(String msg) {
+        super(msg);
+    }
+}
