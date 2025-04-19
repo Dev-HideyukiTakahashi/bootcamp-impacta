f765318fea455ec0a17a8188cde6b5d61d7b7d1e
Hideyuki <dev.hideyukitakahashi@gmail.com>
2025-04-19 05:34:39 -0300

    add buscar por id - Tag

--- backend/src/main/java/br/com/impacta/boacao/controller/TagController.java
+++ backend/src/main/java/br/com/impacta/boacao/controller/TagController.java
@@ -1,14 +1,17 @@
 package br.com.impacta.boacao.controller;
 
-import br.com.impacta.boacao.dto.TagDTO;
-import br.com.impacta.boacao.service.TagService;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import br.com.impacta.boacao.dto.TagDTO;
+import br.com.impacta.boacao.service.TagService;
 
 @RestController
 @RequestMapping(path = "/tags")
@@ -21,9 +24,18 @@ public class TagController {
         this.tagService = service;
     }
 
-    public ResponseEntity<List<TagDTO>> buscarTodos(){
+    @GetMapping
+    public ResponseEntity<List<TagDTO>> buscarTodos() {
         log.info("Iniciando busca de todas as tags.");
         List<TagDTO> dto = tagService.buscarTodos();
+
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<TagDTO> buscarPorId(@PathVariable Integer id) {
+        log.info("Iniciando busca de tag id {}.", id);
+        TagDTO dto = tagService.buscarPorId(id);
 
         return ResponseEntity.ok(dto);
     }
--- backend/src/main/java/br/com/impacta/boacao/service/TagService.java
+++ backend/src/main/java/br/com/impacta/boacao/service/TagService.java
@@ -1,15 +1,17 @@
 package br.com.impacta.boacao.service;
 
-import br.com.impacta.boacao.dto.TagDTO;
-import br.com.impacta.boacao.entities.Tag;
-import br.com.impacta.boacao.mapper.TagMapper;
-import br.com.impacta.boacao.repository.TagRepository;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import br.com.impacta.boacao.dto.TagDTO;
+import br.com.impacta.boacao.entities.Tag;
+import br.com.impacta.boacao.exception.RecursoNaoEncontrado;
+import br.com.impacta.boacao.mapper.TagMapper;
+import br.com.impacta.boacao.repository.TagRepository;
 
 @Service
 public class TagService {
@@ -22,12 +24,21 @@ public class TagService {
     }
 
     @Transactional(readOnly = true)
-    public List<TagDTO> buscarTodos(){
+    public List<TagDTO> buscarTodos() {
         List<Tag> lista = tagRepository.findAll();
         log.info("Busca de todas as tags concluída com sucesso.");
 
         return lista.stream()
                 .map(tag -> TagMapper.paraDTO(tag))
                 .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public TagDTO buscarPorId(Integer id) {
+        Tag entidade = tagRepository.findById(id)
+                .orElseThrow(() -> new RecursoNaoEncontrado("Tag de não encontrada! Id: " + id));
+        log.info("Busca de tag realizada com sucesso! Id: {}", id);
+
+        return TagMapper.paraDTO(entidade);
     }
 }
