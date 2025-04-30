fc2df869769c8e4f13283a3319aef2878b3692df
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-04-30 20:15:36 -0300

    feat: quem-somos component

---
+++ frontend/src/app/features/quem-somos/quem-somos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+
+@Component({
+  selector: 'app-quem-somos',
+  imports: [HeaderComponent],
+  templateUrl: './quem-somos.component.html',
+  styleUrl: './quem-somos.component.scss',
+})
+export class QuemSomosComponent {}
