4361a532dca3964ac1d9e3a7bc9d35a987c1ce84
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-04-30 20:16:27 -0300

    feat: home-ong component

---
+++ frontend/src/app/features/home-ong/home-ong.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+
+@Component({
+  selector: 'app-home-ong',
+  imports: [HeaderComponent],
+  templateUrl: './home-ong.component.html',
+  styleUrl: './home-ong.component.scss',
+})
+export class HomeOngComponent {}
