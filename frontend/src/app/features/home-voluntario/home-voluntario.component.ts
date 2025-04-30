9bee55044645cf9238b1150b75d0969af99b37b1
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-04-30 20:16:04 -0300

    feat: home-voluntario component

---
+++ frontend/src/app/features/home-voluntario/home-voluntario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+
+@Component({
+  selector: 'app-home-voluntario',
+  imports: [HeaderComponent],
+  templateUrl: './home-voluntario.component.html',
+  styleUrl: './home-voluntario.component.scss',
+})
+export class HomeVoluntarioComponent {}
