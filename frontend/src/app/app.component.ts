808ea2774344edcbee90f855597ba85b9241a1c6
thurler142 <thurler9@gmail.com>
2025-05-03 15:38:07 -0300

    Componente Footer
    Implementado o componente footer e adicionado a tag app-footer no componente home.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true, // 👈 isso é importante se estiver usando standalone aqui também
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
