f9bad2c8c818d05b44227d752ff7d080dcb30768
mariizanardii <mariana.zanardi95@outlook.com>
2025-05-11 19:47:57 -0300

    Merge cadastro de voluntario front

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -2,16 +2,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
-<<<<<<< HEAD
-@Component({
-  selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent],
-=======
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-home',
   imports: [HeaderComponent, FooterComponent, RouterModule],
->>>>>>> origin/feature/cadastro
   standalone: true, // 👈 ESSENCIAL
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
