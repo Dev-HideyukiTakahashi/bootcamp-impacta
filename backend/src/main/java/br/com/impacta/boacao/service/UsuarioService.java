fdeb9217ac5d4347cbd457771a0f69bb110fb0d6
Hideyuki <dev.hideyukitakahashi@gmail.com>
2025-06-10 04:28:43 -0300

    feat: metodo para buscar usuario autenticado

--- backend/src/main/java/br/com/impacta/boacao/service/UsuarioService.java
+++ backend/src/main/java/br/com/impacta/boacao/service/UsuarioService.java
@@ -1,8 +1,11 @@
 package br.com.impacta.boacao.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 import br.com.impacta.boacao.entity.Usuario;
@@ -26,5 +29,21 @@ public class UsuarioService implements UserDetailsService {
             return usuario;
         else
             throw new UsernameNotFoundException("Username not found");
+    }
+
+    /*
+     * metodo que retorna o usuario ja autenticado
+     * ao fazer a request, busca o username(email) por dados do jwt
+     */
+    protected Usuario getUsuarioAutenticado() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+
+            return usuarioRepository.findByEmail(username);
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("Email não encontrado");
+        }
     }
 }
