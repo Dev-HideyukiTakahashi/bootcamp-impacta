71d85653ea1e9eaa99b0097ded5d7d1556412581
thurler142 <thurler9@gmail.com>
2025-07-12 17:16:47 -0300

    feat: add home pages for ONG, volunteer, and initial screen

--- frontend/src/app/features/home-ong/home-ong.component.ts
+++ frontend/src/app/features/home-ong/home-ong.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home-ong',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home-ong.component.html',
   styleUrl: './home-ong.component.scss',
 })
--- frontend/src/app/features/home-voluntario/home-voluntario.component.ts
+++ frontend/src/app/features/home-voluntario/home-voluntario.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home-voluntario',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home-voluntario.component.html',
   styleUrl: './home-voluntario.component.scss',
 })
