7dc54bafa7339de6ea1e834c7e61cdb0f6f543a0
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-07-14 20:52:14 -0300

    fix: redirecionamento btn home (voluntario/ong)

--- frontend/src/app/features/home-ong/home-ong.component.ts
+++ frontend/src/app/features/home-ong/home-ong.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home-ong',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './home-ong.component.html',
   styleUrl: './home-ong.component.scss',
 })
--- frontend/src/app/features/home-voluntario/home-voluntario.component.ts
+++ frontend/src/app/features/home-voluntario/home-voluntario.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home-voluntario',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './home-voluntario.component.html',
   styleUrl: './home-voluntario.component.scss',
 })
-export class HomeVoluntarioComponent {}
+export class HomeVoluntarioComponent {
+  router = inject(Router);
+
+  navigate() {
+    this.router.navigate(['/perfil-voluntario']);
+  }
+}
