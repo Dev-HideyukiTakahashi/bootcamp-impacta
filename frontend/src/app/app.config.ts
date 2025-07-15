d5528ced3d9dc5a1ddcd00a477a72e173645527e
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-07-15 01:57:34 -0300

    fix: correção de warnings

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideNgxMask } from 'ngx-mask';
@@ -7,7 +7,7 @@ import { authInterceptor } from './core/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withInterceptors([authInterceptor])), // Fornecendo HttpClient para toda a aplicação
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])), // Fornecendo HttpClient para toda a aplicação
     provideRouter(routes), // Fornecendo as rotas
     provideNgxMask(),
   ],
